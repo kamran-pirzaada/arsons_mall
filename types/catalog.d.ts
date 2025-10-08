4e68c08d0dab76ad5fba16df2c7ac891306344f3
kamran-pirzaada <kamranpirzaada@gmail.com>
2025-10-08 17:02:48 +0500

    Implement catalog and product fetching with UI components for the homepage

---
+++ types/catalog.d.ts
@@ -0,0 +1,13 @@
+import { IProduct } from './product';
+
+export interface ICatalog {
+  id: number;
+
+  localizeInfos?: {
+    title?: string;
+  };
+
+  catalogProducts: {
+    items: IProduct[];
+  };
+}
---
+++ types/product.d.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+id: any;
+
+  localizeInfos: { title: any };
+
+  price: any;
+
+  attributeValues: {
+    p_image: { value: { downloadLink: any } };
+
+    p_title: { value: any };
+  };
+}
